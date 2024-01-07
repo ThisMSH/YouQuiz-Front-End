595fff7733f08feea7516f4a17943b9465d49b81
El Mahdi S.H. <midoyugi@hotmail.com>
2024-01-07 17:01:39 +0100

    Creating Answers service

--- src/app/services/answers/answers.service.ts
+++ src/app/services/answers/answers.service.ts
@@ -1,9 +1,55 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AnswerRequest } from 'src/app/models/answer/answer-request';
+import { AnswerResponse } from 'src/app/models/answer/answer-response';
+import { ListResponse } from 'src/app/models/response/list-response';
+import { Response } from 'src/app/models/response/response';
+import { SortOrder } from 'src/app/types/types';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class AnswersService {
+    private http = inject(HttpClient);
+    url = environment.apiUrl;
 
-  constructor() { }
+    getAnswer(id: number): Observable<Response<AnswerResponse>> {
+        return this.http.get<Response<AnswerResponse>>(
+            `${this.url}/answers/${id}`,
+        );
+    }
+
+    getAllAnswers(
+        size: number,
+        sortBy: string,
+        sortOrder: SortOrder,
+        page: number = 0,
+        answer: string = '',
+    ): Observable<ListResponse<AnswerResponse>> {
+        return this.http.get<ListResponse<AnswerResponse>>(
+            `${this.url}/answers?page=${page}&size=${size}&sortBy=${sortBy}&sortOrder=${sortOrder}&text=${answer}`,
+        );
+    }
+
+    createAnswer(answer: AnswerRequest): Observable<Response<AnswerResponse>> {
+        return this.http.post<Response<AnswerResponse>>(
+            `${this.url}/answers/add`,
+            answer,
+        );
+    }
+
+    updateAnswer(answer: AnswerRequest): Observable<Response<AnswerResponse>> {
+        return this.http.put<Response<AnswerResponse>>(
+            `${this.url}/answers/update`,
+            answer,
+        );
+    }
+
+    deleteAnswer(id: number): Observable<Response<AnswerResponse>> {
+        return this.http.delete<Response<AnswerResponse>>(
+            `${this.url}/answers/${id}`,
+        );
+    }
 }
