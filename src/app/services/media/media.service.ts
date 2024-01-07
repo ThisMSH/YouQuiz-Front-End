d378c0056587b2322502c1230196034a4f03a97a
El Mahdi S.H. <midoyugi@hotmail.com>
2024-01-07 17:24:39 +0100

    Creating Media service

--- src/app/services/media/media.service.ts
+++ src/app/services/media/media.service.ts
@@ -1,9 +1,8 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable, inject, signal } from '@angular/core';
-import { Observable, catchError, tap, throwError } from 'rxjs';
-import { MediaDTO } from 'src/app/interfaces/media/media-dto';
-import { MediaRequest } from 'src/app/interfaces/media/media-request';
-import { Response } from 'src/app/interfaces/response/response';
+import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { MediaResponse } from 'src/app/models/media/media-response';
+import { Response } from 'src/app/models/response/response';
 import { environment } from 'src/environments/environment.development';
 
 @Injectable({
@@ -11,49 +10,24 @@ import { environment } from 'src/environments/environment.development';
 })
 export class MediaService {
     private http = inject(HttpClient);
-    media = signal<MediaDTO | undefined>(undefined);
     url = environment.apiUrl;
 
-    getMedia(id: number): Observable<Response<MediaDTO>> {
-        return this.http
-            .get<Response<MediaDTO>>(`${this.url}/medias/${id}`)
-            .pipe(
-                tap((res) => {
-                    if (res.status !== 200) {
-                        throw new Error(`An error occured: ${res.message}`);
-                    }
-
-                    this.media.set(res.data);
-                }),
-                catchError((err) => throwError(() => err)),
-            );
+    getMedia(id: number): Observable<Response<MediaResponse>> {
+        return this.http.get<Response<MediaResponse>>(
+            `${this.url}/media/${id}`,
+        );
     }
 
-    createMedia(media: FormData): Observable<Response<MediaDTO>> {
-        return this.http
-            .post<Response<MediaDTO>>(`${this.url}/medias/add`, media)
-            .pipe(
-                tap((res) => {
-                    if (res.status !== 201) {
-                        throw new Error(`An error occured: ${res.message}`);
-                    }
-
-                    this.media.set(res.data);
-                }),
-                catchError((err) => throwError(() => err)),
-            );
+    createMedia(media: FormData): Observable<Response<MediaResponse>> {
+        return this.http.post<Response<MediaResponse>>(
+            `${this.url}/media/add`,
+            media,
+        );
     }
 
-    deleteMedia(id: number): Observable<Response<number>> {
-        return this.http
-            .delete<Response<number>>(`${this.url}/medias/${id}`)
-            .pipe(
-                tap((res) => {
-                    if (res.status !== 200) {
-                        throw new Error(`An error occured: ${res.message}`);
-                    }
-                }),
-                catchError((err) => throwError(() => err)),
-            );
+    deleteMedia(id: number): Observable<Response<MediaResponse>> {
+        return this.http.delete<Response<MediaResponse>>(
+            `${this.url}/media/${id}`,
+        );
     }
 }
