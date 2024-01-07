62ed9a2f0a19f4b17c9f5157084604fa09814233
El Mahdi S.H. <midoyugi@hotmail.com>
2024-01-07 17:12:03 +0100

    Creating AnswerValidation service

--- src/app/services/answer-validations/answer-validation.service.ts
+++ src/app/services/answer-validations/answer-validation.service.ts
@@ -1,9 +1,72 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AnswerValidationRequest } from 'src/app/models/answer-validation/answer-validation-request';
+import { AnswerValidationResponse } from 'src/app/models/answer-validation/answer-validation-response';
+import { Response } from 'src/app/models/response/response';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class AnswerValidationService {
+    private http = inject(HttpClient);
+    url = environment.apiUrl;
 
-  constructor() { }
+    getAnswerValidationById(
+        id: number,
+    ): Observable<Response<AnswerValidationResponse>> {
+        return this.http.get<Response<AnswerValidationResponse>>(
+            `${this.url}/answers-assignment/pk/${id}`,
+        );
+    }
+
+    getAnswerValidationByFk(
+        questionId: number,
+        answerId: number,
+    ): Observable<Response<AnswerValidationResponse>> {
+        return this.http.get<Response<AnswerValidationResponse>>(
+            `${this.url}/answers-assignment/fk/${questionId}/${answerId}`,
+        );
+    }
+
+    getAnswerValidationByQuestion(
+        questionId: number,
+    ): Observable<Response<AnswerValidationResponse[]>> {
+        return this.http.get<Response<AnswerValidationResponse[]>>(
+            `${this.url}/answers-assignment/by-question/${questionId}`,
+        );
+    }
+
+    getAnswerValidationByAnswer(
+        answerId: number,
+    ): Observable<Response<AnswerValidationResponse[]>> {
+        return this.http.get<Response<AnswerValidationResponse[]>>(
+            `${this.url}/answers-assignment/by-answer/${answerId}`,
+        );
+    }
+
+    assignAnswer(
+        answerValidation: AnswerValidationRequest,
+    ): Observable<Response<AnswerValidationResponse>> {
+        return this.http.post<Response<AnswerValidationResponse>>(
+            `${this.url}/answers-assignment/add`,
+            answerValidation,
+        );
+    }
+
+    deleteById(id: number): Observable<Response<AnswerValidationResponse>> {
+        return this.http.delete<Response<AnswerValidationResponse>>(
+            `${this.url}/answers-assignment/pk/${id}`,
+        );
+    }
+
+    deleteByFk(
+        questionId: number,
+        answerId: number,
+    ): Observable<Response<AnswerValidationResponse>> {
+        return this.http.delete<Response<AnswerValidationResponse>>(
+            `${this.url}/answers-assignment/fk/${questionId}/${answerId}`,
+        );
+    }
 }
