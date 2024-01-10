26a6296c93903900f78591cd3301960817421929
El Mahdi S.H. <midoyugi@hotmail.com>
2024-01-10 11:51:25 +0100

    Creating all actions for Question actions

--- src/app/store/question/question.actions.ts
+++ src/app/store/question/question.actions.ts
@@ -1,5 +1,7 @@
 import { createAction, props } from '@ngrx/store';
 import { ListResponse } from 'src/app/interfaces/response/list-response';
+import { Response } from 'src/app/interfaces/response/response';
+import { QuestionRequest } from 'src/app/models/question/question-request';
 import { QuestionResponse } from 'src/app/models/question/question-response';
 import { QuestionType, SortOrder } from 'src/app/types/types';
 
@@ -18,11 +20,72 @@ export const getAllQuestions = createAction(
         };
     }>(),
 );
+
 export const getAllQuestionsSucceed = createAction(
     '[Question] Get All Questions Successfully',
     props<ListResponse<QuestionResponse>>(),
 );
+
 export const getAllQuestionsFailed = createAction(
     '[Question] Get All Questions Failed',
+    props<{ error: string }>(),
+);
+
+export const getQuestion = createAction(
+    '[Question] Get Question',
+    props<{ id: number }>(),
+);
+
+export const getQuestionSucceed = createAction(
+    '[Question] Get Question Successfully',
+    props<Response<QuestionResponse>>(),
+);
+
+export const getQuestionFailed = createAction(
+    '[Question] Get Question Failed',
+    props<{ error: string }>(),
+);
+
+export const createQuestion = createAction(
+    '[Question] Create Question',
+    props<{ question: QuestionRequest }>(),
+);
+
+export const createQuestionSucceed = createAction(
+    '[Question] Create Question Successfully',
+    props<Response<QuestionResponse>>(),
+);
+
+export const createQuestionFailed = createAction(
+    '[Question] Create Question Failed',
+    props<{ error: string }>(),
+);
+
+export const updateQuestion = createAction(
+    '[Question] Update Question',
+    props<{ question: QuestionRequest }>(),
+);
+
+export const updateQuestionSucceed = createAction(
+    '[Question] Update Question Successfully',
+    props<Response<QuestionResponse>>(),
+);
+
+export const updateQuestionFailed = createAction(
+    '[Question] Update Question Failed',
+    props<{ error: string }>(),
+);
+
+export const deleteQuestion = createAction(
+    '[Question] Delete Question',
+    props<{ id: number }>(),
+);
+
+export const deleteQuestionSucceed = createAction(
+    '[Question] Delete Question Successfully',
+);
+
+export const deleteQuestionFailed = createAction(
+    '[Question] Delete Question Failed',
     props<{ error: string }>(),
 );
